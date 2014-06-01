4017a4a2c0337da6d48c6cb2a07bbb6fd363860b
zmming <zhao.mming@gmail.com>
2014-06-01 20:10:41 +0800

    change class to function

--- markdownlization.py
+++ markdownlization.py
@@ -9,18 +9,63 @@ __author__='zmming'
 
 import markdown
 
-class markdownlization:
-    def convert2html(self,text):
-        html = markdown.markdown(text)
-        return html
+def convert2html(text):
+    html = markdown.markdown(text)
+    return html
 
-    def title(self, text, n):
-        pre='#'*n
-        return pre+text    
+def title(text, n):
+    pre='#'*n
+    tail='\n'
+    return pre+text+tail
+    
+def num(text):
+    pre='1. '
+    tail='\n'
+    return pre+text+tail
+    
+def noNum(text):
+    pre='- '
+    tail='\n'
+    return pre+text+tail
+    
+def italic(text):
+    pre='*'
+    tail='*\n'
+    return pre+text+tail
+    
+def bold(text):
+    pre='**'
+    tail='**\n'
+    return pre+text+tail
+    
+def italicBold(text):
+    pre='***'
+    tail='***\n'
+    return pre+text+tail
+
+def quote(text):
+    pre='>'
+    tail='\n'
+    return pre+text+tail
+
+def pic(picName, description):
+    pre='!['+description+']'
+    tail='('+picName+')\n'
+    return pre+tail
+
+def tab(title, body):
+    pre='|'+'|'.join(title)+'|'+'\n'+('|---------:|'*len(title))+'\n'
+    con=''
+    for line in body:
+        con+=('|'+'|'.join(line)+'|\n')
+    return pre+con    
 
 
 if __name__=='__main__':
-    md=markdownlization()
-    text='#test'
-    html=md.convert2html(text)
+    text=title('test', 3)
+    text+=italicBold('context')
+    t=['tab1','tab2','tab3']
+    b=[['n1','n2','n3'],['m1','m2','m3']]
+    text+=tab(t,b)
+    html=convert2html(text)
     print html
